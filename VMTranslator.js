8173f9e36946aaab147233e8cacb115d5e028e58
Nykolai Topchyi <nodetempest@gmail.com>
2022-07-02 15:08:00 +0300

    implement VMTranslator for MemoryAccess

--- VMTranslator.js
+++ VMTranslator.js
@@ -165,15 +165,104 @@ const calcOperationsMap = {
 };
 
 // prettier-ignore
+const createGeneralPushPopHandler = ptr => ({
+  push: (value) =>  [
+                      `@${value}`,    // D = *(ptr + value)
+                      "D=A",
+                      `@${ptr}`,
+                      "A=A+D",
+                      "D=M",
+                      "@SP",    // *SP = D
+                      "A=M",
+                      "M=D",
+                      "@SP",    // SP++
+                      "M=M+1",
+                    ].join("\r\n"),
+
+  pop: (value) =>  [
+                    `@${ptr}`,    // temp_ptr = ptr + value
+                    "D=A",
+                    `@${value}`,
+                    "D=D+A",
+                    "@temp_ptr",
+                    "M=D",
+                    "@SP",    // SP--
+                    "M=M-1",
+                    "A=M",    // D = *SP
+                    "D=M",
+                    "@temp_ptr",    // *temp_ptr = D
+                    "A=M",
+                    "M=D",
+                  ].join("\r\n"),
+})
+
+// prettier-ignore
 const pushPopMap = {
   constant: {
     push: (value) =>  [
-                        "@SP",
+                        "@SP",    // *SP = value
                         "A=M",
                         `M=${value}`,
-                        "@SP",
+                        "@SP",    // SP++
                         "M=M+1",
-                      ].join("\r\n")
+                      ].join("\r\n"),
+  },
+
+  local: createGeneralPushPopHandler("LCL"),
+  argument: createGeneralPushPopHandler("ARG"),
+  this: createGeneralPushPopHandler("THIS"),
+  that: createGeneralPushPopHandler("THAT"),
+  temp: createGeneralPushPopHandler("5"),
+
+  pointer: {
+    push: (value) =>  [
+                        "@PUSH_THIS",   // value == 0 ? A = THIS : A = THAT
+                        `${value};JEQ`,
+                        "@THAT",
+                        "(PUSH_THIS)",    // *SP = THIS/THAT
+                        "@THIS",
+                        "D=M",
+                        "@SP",
+                        "A=M",
+                        "M=D",
+                        "@SP",    // SP++
+                        "M=M+1",
+                      ].join("\r\n"),
+
+    pop: (value) =>  [
+                        "@SP",    // SP--
+                        "M=M-1",   
+                        "@SP",    // D = *SP
+                        "A=M",
+                        "D=M",
+                        "@POP_THIS",    // value == 0 ? A = THIS : A = THAT
+                        `${value};JEQ`,
+                        "@THAT",
+                        "(POP_THIS)",
+                        "@THIS",
+                        "M=D",    // THIS/THAT = D
+                      ].join("\r\n"),
+  },
+
+  static: {
+    push: (value, fileName) =>  [
+                                  `@${[fileName, value].join(".")}`,    // D = @Foo.i
+                                  "D=M",
+                                  "@SP",    // *SP = D
+                                  "A=M",
+                                  "M=D",
+                                  "@SP",    // SP++
+                                  "M=M+1",
+                                ].join("\r\n"),
+
+    pop: (value, fileName) =>  [
+                                  "@SP",    // SP--
+                                  "M=M-1",
+                                  "A=M",    // D = *SP
+                                  "D=M",
+                                  `@${[fileName, value].join(".")}`,    // @Foo.i = D
+                                  "M=D",
+                                ].join("\r\n"),
   },
 };
 
@@ -181,34 +270,35 @@ const removeCommentsAndWhitespaces = (lines) => {
   return lines.map((line) => line.split("//")[0].trim()).filter(Boolean);
 };
 
-const translateInstructions = (lines) => {
+const translateInstructions = (lines, fileName) => {
   return lines.map((instruction) => {
     if (instruction in calcOperationsMap) {
       return calcOperationsMap[instruction];
     } else {
       const [stackOperation, memorySegment, value] = instruction.split(" ");
 
-      return pushPopMap[memorySegment][stackOperation](value);
+      return pushPopMap[memorySegment][stackOperation](value, fileName);
     }
   });
 };
 
-const processLines = (lines) => {
+const processLines = (lines, fileName) => {
   lines = removeCommentsAndWhitespaces(lines);
-  lines = translateInstructions(lines);
+  lines = translateInstructions(lines, fileName);
 
   return lines;
 };
 
 const main = () => {
   const pathToFileArg = path.resolve(process.argv.slice(2)[0]);
+  const fileName = path.parse(pathToFileArg).name;
 
   const fileContent = fs.readFileSync(pathToFileArg).toString();
   const lines = fileContent.split("\r\n");
 
   fs.writeFileSync(
     pathToFileArg.replace(".vm", ".asm"),
-    processLines(lines).join("\r\n")
+    processLines(lines, fileName).join("\r\n")
   );
 };
 
