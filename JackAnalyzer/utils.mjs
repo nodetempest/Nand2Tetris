26252346167e6a703ccbdd6c2c67c7efbd1e3df0
Nykolai Topchyi <nodetempest@gmail.com>
2022-09-04 16:55:39 +0300

    remove unused utils

--- JackAnalyzer/utils.mjs
+++ JackAnalyzer/utils.mjs
@@ -1,17 +1,5 @@
 import fs from "fs";
 import path from "path";
-import crypto from "crypto";
-
-export const genId = () => crypto.randomBytes(16).toString("hex");
-
-export const range = (n) => {
-  return Array.apply(null, { length: n }).map(Number.call, Number);
-};
-
-export const o =
-  (f, g) =>
-  (...args) =>
-    f(g(...args));
 
 export const isDir = (path) => {
   try {
